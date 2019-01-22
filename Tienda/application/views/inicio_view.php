ed1c914a2736fdd5c7ed9b05c49c15f32643a907
alvarorq <alvarorq12@outlook.com>
2019-01-22 20:35:16 +0100

    Añadido bootstrap y cambio de la vista

--- Tienda/application/views/inicio_view.php
+++ Tienda/application/views/inicio_view.php
@@ -5,29 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Tienda PHP - Pruebas</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?= base_url();?>asset/css/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?= base_url();?>asset/bootstrap/css/bootstrap.min.css" />
 </head>
 <body>
-    <h1>Tienda PHP</h1>
-    <a href="<?= base_url();?>index.php/inicio_ctrl/todos">Todos</a>
-    <a href="<?= base_url();?>">Destacados</a>
-    <table border=solid>
-    <tr>
-    <th>Nombre</th>
-    <th>Descripcion</th>
-    <th>Precio</th>
-    <th>Stock</th>
-    <th>Visible</th>
-    </tr>
-    <?php foreach ($productos as $producto) { ?>
-        <tr>
-            <td><?= $producto->nombre ?></td>
-            <td><?= $producto->descripcion ?></td>
-            <td><?= $producto->precio ?></td>
-            <td><?= $producto->stock ?></td>
-            <td><?= $producto->visible ?></td>
-        </tr>
-    <?php } ?>
-    </table>
+    <div class="container">
+        <h1>Tienda PHP</h1>
+        <a href="<?= base_url();?>index.php/inicio_ctrl/todos">Todos</a>
+        <a href="<?= base_url();?>">Destacados</a>
+            
+        <div class="row contenido">
+            
+            <?php foreach ($productos as $producto) { ?>
+                <div class="card col-md-3 col-12" >
+                    <img src="<?= base_url().$producto->imagen; ?>" class="card-img-top" alt="<?= $producto->nombre; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $producto->nombre; ?></h5>
+                        <p class="card-text"><?= $producto->descripcion; ?></p>
+                        <p class="card-text"><?= $producto->precio; ?>€</p>
+                        <a href="#" class="btn btn-primary"> Al carrito</a>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 </body>
 </html>
--- Tienda/application/views/plantillas/menu_categorias.php
+++ Tienda/application/views/plantillas/menu_categorias.php
@@ -1,4 +1,5 @@
-<div>
+<div class="izq">
+    <p><strong>Categorias:</strong></p>
     <ul>
         <?php foreach ($categorias as $categoria) { ?>
         <li><?= $categoria->categoria?></li>
